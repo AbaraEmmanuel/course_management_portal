f77f731f615cca536690064d66bbc41c649adcd2
AbaraEmmanuel <emmanuelabara265@gmail.com>
2026-02-20 22:11:19 +0100

    changed API key

--- content-manager.js
+++ content-manager.js
@@ -1,5 +1,5 @@
 // At the top of content-manager.js
-const API_URL = 'https://jaromind-production-014b.up.railway.app'; // Your Go backend
+const API_URL = 'https://jaromind-production-3060.up.railway.app'; // Your Go backend
 let authToken = localStorage.getItem('authToken');
 let editingCourseId = null;
 
